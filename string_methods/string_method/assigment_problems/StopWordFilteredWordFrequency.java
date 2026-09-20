e6336548f1f6c565afe59ca577fddb585c53dca6
PRANESH M P <pooranipranesh09@gmail.com>
2026-09-20 10:27:19 +0530

    Create StopWordFilteredWordFrequency.java

---
+++ string_methods/string_method/assigment_problems/StopWordFilteredWordFrequency.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class StopWordFilteredWordFrequency {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        feedback = feedback.toLowerCase();
+        feedback = feedback.replace(".", "");
+        feedback = feedback.replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
